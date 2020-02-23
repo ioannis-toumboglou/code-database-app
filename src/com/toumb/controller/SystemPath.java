8cf085ce8294bea86abe157d100da240d091ca35
toumb <ioannis.toumboglou@gmail.com>
2020-02-23 13:21:49 +0200

    Define code files path dynamically in controller

--- src/com/toumb/controller/SystemPath.java
+++ src/com/toumb/controller/SystemPath.java
@@ -1,9 +1,13 @@
 package com.toumb.controller;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class SystemPath {
 	public static String path() {
-		// Define the path in which the code files are stored 
-    	String path = "C:/<path>/eclipse-workspace/code-database-app/WebContent/resources/code-files/";
+		// Define the path in which the code files are stored
+		Path tempPath = FileSystems.getDefault().getPath("").toAbsolutePath();
+    	String path = tempPath + "\\WebContent\\resources\\code-files\\";
 
     	return path;
      }
